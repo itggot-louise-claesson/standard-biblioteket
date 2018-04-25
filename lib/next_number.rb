145153e503596081d96bbd7007803538f6376ab7
itggot-louise-claesson <louise.claesson@itggot.se>
2018-04-25 13:55:33 +0200

    Added "next number"

---
+++ lib/next_number.rb
@@ -0,0 +1,17 @@
+# Public: An Integer as input and gives the next number as output.
+#
+# number - the Integer increase with one.
+#
+# Examples
+#
+#   next_number(5)
+#   # => 6
+#
+# Returns the increased number.
+def next_number(number)
+
+    output = number + 1
+
+    return output
+
+end

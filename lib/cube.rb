f01deb18ff8fd2e74739eadd7202dd0be830514a
itggot-louise-claesson <louise.claesson@itggot.se>
2018-04-25 14:44:40 +0200

    Added "cube"

---
+++ lib/cube.rb
@@ -0,0 +1,15 @@
+# Public: An Integer as input and the Integer to the power of three as the output.
+#
+# number - the Integer that is multiplied by itself three times.
+#
+# Examples
+#
+#   next_number(2)
+#   # => 8
+#
+# Returns the squared number.
+def cube(number)
+
+    number * number * number
+
+end
